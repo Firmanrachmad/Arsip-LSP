72d7b1c0349dbb0d894663fcbb53a537b1bd7ee2
Firmanrachmad <dragobniel@gmail.com>
2022-09-15 21:29:18 +0700

    update ui

---
+++ resources/views/about.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.template')
+@section('content')
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">About Me</h1>
+</div>
+
+
+<div class="row">
+    <div class="col-lg-6">
+        <!-- Basic Card Example -->
+        <div class="card shadow mb-4">
+            <div class="card-body">
+                <div class="row">
+                    <p class="mb-0">Iki Gambar
+                        <div class="col">
+                        <p class="mb-0">Aplikasi ini dibuat oleh:
+                        <p class="mb-0">Nama    : Firman Rachmad Caesar
+                <p class="mb-0">NIM     : 1931710152
+                <p class="mb-0">Tanggal : 15 September 2022
+
+                        </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+@endsection
